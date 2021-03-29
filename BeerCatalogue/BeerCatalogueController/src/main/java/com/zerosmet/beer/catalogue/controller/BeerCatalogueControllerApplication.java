c7bf9761fa65ec6d1fa1462afa272f015fc70fe2
david.bassols <zerosmet@gmail.com>
2021-03-29 09:44:35 +0200

    Base project

---
+++ BeerCatalogue/BeerCatalogueController/src/main/java/com/zerosmet/beer/catalogue/controller/BeerCatalogueControllerApplication.java
@@ -0,0 +1,13 @@
+package com.zerosmet.beer.catalogue.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerCatalogueControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerCatalogueControllerApplication.class, args);
+	}
+
+}
